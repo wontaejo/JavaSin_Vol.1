cb78c53e12e1b024278675bc0bd28f0884ba6dbd
wontaejo <wontaejo87@gmail.com>
2017-01-06 01:07:41 +0900

    2장 Hello Basic Java 출력

---
+++ Chapter02/src/HelloBasicJava.java
@@ -0,0 +1,7 @@
+
+public class HelloBasicJava {
+    public static void main(String[] args) {
+        System.out.println("Hello God of Java !!!!!");
+        System.out.println("I love this book.");
+    }
+}
---
+++ Chapter02/src/Profile.java
@@ -0,0 +1,7 @@
+
+public class Profile {
+	public static void main(String[] args) {
+		System.out.println("My name is Á¶¿øÅÂ");
+		System.out.println("My age is 31");
+	}
+}
