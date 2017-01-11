f92dd0cf2524b262b36545378b508cf84d7317bd
wontaejo <wontaejo87@gmail.com>
2017-01-11 23:34:50 +0900

    6장 - ControlofFlow class ifStatement method : 기본 조건문

---
+++ Chapter06/src/ControlOfFlow.java
@@ -0,0 +1,16 @@
+
+public class ControlOfFlow {
+    public static void main(String[] args) {
+        ControlOfFlow control = new ControlOfFlow();
+        control.ifStatement();
+    }
+    
+    public void ifStatement() {
+        if(true);
+        if(true) System.out.println("It's true");
+        if(true)
+            System.out.println("It's also true.");
+        if(false) System.out.println("It's false");
+    }
+}
+
