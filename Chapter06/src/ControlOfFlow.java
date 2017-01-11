5905faa848304b350fcb1f43a62367e93cc9e894
wontaejo <wontaejo87@gmail.com>
2017-01-11 23:38:51 +0900

    6장 - ControlOfFlow class ifStatement2 method added : 조건문 else

--- Chapter06/src/ControlOfFlow.java
+++ Chapter06/src/ControlOfFlow.java
@@ -2,7 +2,8 @@
 public class ControlOfFlow {
     public static void main(String[] args) {
         ControlOfFlow control = new ControlOfFlow();
-        control.ifStatement();
+        //control.ifStatement();
+        control.ifStatement2();
     }
     
     public void ifStatement() {
@@ -11,6 +12,18 @@ public class ControlOfFlow {
         if(true)
             System.out.println("It's also true.");
         if(false) System.out.println("It's false");
+    }
+    
+    public void ifStatement2() {
+        int intValue = 10;
+        
+        if(intValue > 5) System.out.println("It's bigger than 5.");
+        else System.out.println("It's smaller or equal than 5.");
+        
+        if(intValue <= 5)
+            System.out.println("It's smaller or equal than 5.");
+        else
+            System.out.println("It's bigger than 5.");
     }
 }
 
