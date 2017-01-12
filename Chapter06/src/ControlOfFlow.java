6d9202a611de69e2f4cc50bd6760a551c0c287a8
wontaejo <wontaejo87@gmail.com>
2017-01-12 22:33:29 +0900

    6장 - ControlOfFlow class switchStatement2 method - switch 문 활용

--- Chapter06/src/ControlOfFlow.java
+++ Chapter06/src/ControlOfFlow.java
@@ -6,7 +6,8 @@ public class ControlOfFlow {
         //control.ifStatement2();
         //control.ifStatement3();
         //control.ifStatement4(90);
-        control.switchStatement(1);
+        //control.switchStatement(1);
+        control.switchStatement2(4);
     }
     
     public void control() {
@@ -75,6 +76,31 @@ public class ControlOfFlow {
             default:
                 System.out.println("It is an expensive car.");
                 break;
+        }
+    }
+    
+    public void switchStatement2(int month) {
+        switch(month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            System.out.println(month + " has 31 days.");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            System.out.println(month + " has 30 days.");
+            break;
+        case 2:
+            System.out.println(month + "has 28 or 29 days.");
+            break;
+        default:
+            System.out.println(month + " is not a mont");
         }
     }
 }
