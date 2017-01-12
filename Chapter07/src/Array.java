b0e3b4fc772b5e7b1893f76c4688a3ad32b14ec4
wontaejo <wontaejo87@gmail.com>
2017-01-12 23:48:38 +0900

    7장 - Array class primitiveTypes method - 배열 선언 시 기본자료형 기본할당값

--- Chapter07/src/Array.java
+++ Chapter07/src/Array.java
@@ -2,7 +2,8 @@
 public class Array {
     public static void main(String[] args) {
         Array array = new Array();
-        array.init();
+        //array.init();
+        array.primitiveTypes();
     }
     
     public void init() {
@@ -16,5 +17,25 @@ public class Array {
         lottoNumbers[6] = 2;
         // lottoNumbers[7] = 9; // 예외발생
         // System.out.println(lottoNumbers[7]); // 예외발생
+    }
+    
+    public void primitiveTypes() {
+        byte[] byteArray = new byte[1];
+        short[] shortArray = new short[1];
+        int[] intArray = new int[1];
+        long[] longArray = new long[1];
+        float[] floatArray = new float[1];
+        double[] doubleArray = new double[1];
+        char[] charArray = new char[1];
+        boolean[] booleanArray = new boolean[1];
+        
+        System.out.println("byteArray[0]" + byteArray[0]);
+        System.out.println("shortArray[0]" + shortArray[0]);
+        System.out.println("intArray[0]" + intArray[0]);
+        System.out.println("longArray[0]" + longArray[0]);
+        System.out.println("floatArray[0]" + floatArray[0]);
+        System.out.println("doubleArray[0]" + doubleArray[0]);
+        System.out.println("charArray[0]" + charArray[0]);
+        System.out.println("booleanArray[0]" + booleanArray[0]);
     }
 }
