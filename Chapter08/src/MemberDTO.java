e220ef4597db6f6be9ccfe08a2a6125a8e4b611a
wontaejo <wontaejo87@gmail.com>
2017-01-27 23:49:44 +0900

    8장 - MemberDTO added

---
+++ Chapter08/src/MemberDTO.java
@@ -0,0 +1,12 @@
+
+public class MemberDTO {
+    public String name;
+    public String phone;
+    public String email;
+
+    public MemberDTO getMemberDTO() {
+        MemberDTO dto = new MemberDTO();
+        return dto;
+    }
+}
+
