f40edde3161c08ca877f9227adc606de15dc4d9f
wontaejo <wontaejo87@gmail.com>
2017-01-28 12:44:03 +0900

    8장 - ReferenceTypes class retrun Type method added

--- Chapter08/src/ReferenceTypes.java
+++ Chapter08/src/ReferenceTypes.java
@@ -30,4 +30,19 @@ public class ReferenceTypes {
     
     public void print(String stringData, int intData) {
     }
+    
+    public int intReturn() {
+        int returnInt = 0;
+        return returnInt;      
+    }
+    
+    public int[] intArrayReturn() {
+        int[] returnArray = new int[10];
+        return returnArray;
+    }
+    
+    public String stringReturn() {
+        String returnString = "Return value";
+        return returnString;
+    }
 }
