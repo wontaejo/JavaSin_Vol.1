5a3ce553eb130d8c74b6d5efe8ee13d461f77279
wontaejo <wontaejo87@gmail.com>
2017-01-28 12:36:25 +0900

    8장 - ReferenceTypes class print method : method overloading

--- Chapter08/src/ReferenceTypes.java
+++ Chapter08/src/ReferenceTypes.java
@@ -11,4 +11,23 @@ public class ReferenceTypes {
     public static void main(String[] args) {
         ReferenceTypes reference = new ReferenceTypes();
     }
+    
+    public void makeMemberObject() {
+        MemberDTO dto1 = new MemberDTO();
+        MemberDTO dto2 = new MemberDTO("Sangmain");
+        MemberDTO dto3 = new MemberDTO("Sangmain", "010XXXXYYYY");
+        MemberDTO dto4 = new MemberDTO("Sangmain", "010XXXXYYYY", "god@godofjava.com");
+    }
+    
+    public void print(int data) {
+    }
+    
+    public void print(String data) {
+    }
+    
+    public void print(int intData, String stringData) {
+    }
+    
+    public void print(String stringData, int intData) {
+    }
 }
