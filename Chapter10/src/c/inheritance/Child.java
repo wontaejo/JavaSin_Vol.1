01bf42cf6ba82b15273f9579ecc2ea56d77ce0b1
wontaejo <wontaejo87@gmail.com>
2017-01-29 11:17:53 +0900

    상속관계에서의 method overriding

--- Chapter10/src/c/inheritance/Child.java
+++ Chapter10/src/c/inheritance/Child.java
@@ -2,6 +2,17 @@ package c.inheritance;
 
 public class Child extends Parent {
     public Child() {
-        System.out.println("Child Constructor");
+        super(null);
+        //System.out.println("Child Constructor");
+    }
+    public Child(String name) {
+        super(name);
+        //System.out.println("Child(String) Constructor");
+    }
+    public void printAge() {
+        //System.out.println("printAge() - 18 month");
+    }
+    public void printName() {
+        System.out.println("printName() - Child");
     }
 }
--- Chapter10/src/c/inheritance/Parent.java
+++ Chapter10/src/c/inheritance/Parent.java
@@ -1,10 +1,21 @@
 package c.inheritance;
 
 public class Parent {
+    /*
     public Parent() {
         System.out.println("Parent Constructor");
     }
+    */
+    public Parent(String name) {
+        System.out.println("Parent(String) Constructor");
+    }
+    /*
+    public Parent(Inheritance obj) {
+        System.out.println("Parent(Inheritance) Constructor");
+    }
+    */
     public void printName() {
         System.out.println("PrintName() - Parent");
     }
+    
 }
