4fb829c5d7bff8b645de79c5a70a1fb2dbdd1e67
wontaejo <wontaejo87@gmail.com>
2017-01-30 22:09:54 +0900

    ExceptionSample class finallySample method - finally

--- Chapter14/src/c/exception/ExceptionSample.java
+++ Chapter14/src/c/exception/ExceptionSample.java
@@ -4,7 +4,8 @@ public class ExceptionSample {
     
     public static void main(String[] args) {
         ExceptionSample sample = new ExceptionSample();
-        sample.arrayOutOfBounds();
+        //sample.arrayOutOfBounds();
+        sample.finallySample();
     }
     
     public void arrayOutOfBounds() {
@@ -15,6 +16,18 @@ public class ExceptionSample {
         } catch (Exception e) {
             //System.out.println("Exception occured.");
             System.out.println(intArray.length);
+        }
+        System.out.println("This code should run.");
+    }
+    
+    public void finallySample() {
+        int[] intArray = new int[5];
+        try {
+            System.out.println(intArray[4]);
+        } catch (Exception e) {
+            System.out.println(intArray.length);
+        } finally {
+            System.out.println("Here is finally");
         }
         System.out.println("This code should run.");
     }
