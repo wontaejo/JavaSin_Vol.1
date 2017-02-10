9694c9e5e318b0e6a7b2efa2d35d9d13ca2b2a1f
wontaejo <wontaejo87@gmail.com>
2017-02-10 22:15:11 +0900

    13장 NestedSample class added - 내부 클래스 객체 생성 예

---
+++ Chapter13/src/c/inner/NestedSample.java
@@ -0,0 +1,15 @@
+package c.inner;
+
+public class NestedSample {
+    public static void main(String[] args) {
+        NestedSample sample = new NestedSample();
+        sample.makeStaticNestedObject();
+    }
+    
+    public void makeStaticNestedObject() {
+        OuterOfStatic.StaticNested staticNested = new OuterOfStatic.StaticNested();
+        staticNested.setValue(3);
+        System.out.println(staticNested.getValue());
+    }
+    
+}
