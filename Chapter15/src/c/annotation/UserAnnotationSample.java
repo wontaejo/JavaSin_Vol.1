f325267a0c5e64c228fbfa746b9f9d09bf94bcbb
wontaejo <wontaejo87@gmail.com>
2017-02-11 22:32:00 +0900

    Annotation 값 확인

--- Chapter15/src/c/annotation/UserAnnotationSample.java
+++ Chapter15/src/c/annotation/UserAnnotationSample.java
@@ -1,21 +1,33 @@
 package c.annotation;
 
+import java.lang.reflect.Method;
+
 public class UserAnnotationSample {
     @UserAnnotation(number=0)
     public static void main(String[] args) {
         UserAnnotationSample sample = new UserAnnotationSample();
+        sample.checkAnnotations(UserAnnotationSample.class);
     }
-    
     @UserAnnotation(number=1)
     public void annotationSample1() {
-        
     }
     @UserAnnotation(number=2, text="second")
     public void annotationSample2() {
-        
     }
     @UserAnnotation(number=3, text="third")
     public void annotationSample3() {
-        
+    }
+    public void checkAnnotations(Class useClass) {
+        Method[] method = useClass.getDeclaredMethods();
+        for(Method tempMethod:method) {
+            UserAnnotation annotation = tempMethod.getAnnotation(UserAnnotation.class);
+            if(annotation != null) {
+                int number = annotation.number();
+                String text = annotation.text();
+                System.out.println(tempMethod.getName() + "() : number =" + number + " text=" + text);
+            } else {
+                System.out.println(tempMethod.getName() + "() : annotation is null.");
+            }
+        }
     }
 }
