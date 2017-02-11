31c68e184d859db77b7ef17a0af1dfd403ae3073
wontaejo <wontaejo87@gmail.com>
2017-02-11 20:19:30 +0900

    14장 ExceptionSample class modified try catch 완료 후 구문 실행

--- Chapter14/src/c/exception/ExceptionSample.java
+++ Chapter14/src/c/exception/ExceptionSample.java
@@ -3,15 +3,15 @@ package c.exception;
 public class ExceptionSample {
     public static void main(String[] args) {
         ExceptionSample sample = new ExceptionSample();
-        
+        sample.arrayOutOfBounds();
     }
     public void arrayOutOfBounds() {
         int[] intArray = new int[5];
         try {
             System.out.println(intArray[5]);
-            System.out.println("This code should run.");
         } catch (Exception e) {
-            
+            System.out.println("Exception occured.");
         }
+        System.out.println("This code should run.");
     }
 }
