35696feeef109941079d4f7fb7f6d84be6af1b8f
wontaejo <wontaejo87@gmail.com>
2017-02-11 20:15:48 +0900

    14장 ExceptionSample class modified 예외 발생 시 다음 구분 실행 여부

--- Chapter14/src/c/exception/ExceptionSample.java
+++ Chapter14/src/c/exception/ExceptionSample.java
@@ -9,6 +9,7 @@ public class ExceptionSample {
         int[] intArray = new int[5];
         try {
             System.out.println(intArray[5]);
+            System.out.println("This code should run.");
         } catch (Exception e) {
             
         }
