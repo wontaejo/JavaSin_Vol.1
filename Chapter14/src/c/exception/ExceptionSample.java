53b36370b7466cd9b93a87f099825dcfa5d61177
wontaejo <wontaejo87@gmail.com>
2017-02-11 20:22:05 +0900

    14장 ExceptionSample class modified try catch 변수 범위

--- Chapter14/src/c/exception/ExceptionSample.java
+++ Chapter14/src/c/exception/ExceptionSample.java
@@ -6,11 +6,11 @@ public class ExceptionSample {
         sample.arrayOutOfBounds();
     }
     public void arrayOutOfBounds() {
-        int[] intArray = new int[5];
         try {
+            int[] intArray = new int[5];
             System.out.println(intArray[5]);
         } catch (Exception e) {
-            System.out.println("Exception occured.");
+            System.out.println(intArray.length);
         }
         System.out.println("This code should run.");
     }
